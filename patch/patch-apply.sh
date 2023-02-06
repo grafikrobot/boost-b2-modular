6ded70eba48ec332d103162809d0af6c73afe262
Rene Rivera <grafikrobot@gmail.com>
2023-02-05 22:36:24 -0600

    Add build.jam linking to patch-apply.

--- patch/patch-apply.sh
+++ patch/patch-apply.sh
@@ -10,10 +10,13 @@ pwd
 cd "${patchdir}/libs"
 submods=`ls -1`
 for submod in ${submods} ; do
+    cd "${rootdir}/libs/${submod}"
+    pwd
     if test -s "${patchdir}/libs/${submod}/patch.diff" ; then
-        cd "${rootdir}/libs/${submod}"
-        pwd
         patch -uN -p1 "--input=${patchdir}/libs/${submod}/patch.diff"
+    fi
+    if test -s "${patchdir}/libs/${submod}/build.jam" ; then
+        ln -s "${patchdir}/libs/${submod}/build.jam" build.jam
     fi
 done
 cd "${rootdir}"

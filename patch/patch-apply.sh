ef6b8d19bd75993b2444d1499133b756bf9eddb0
Rene Rivera <grafikrobot@gmail.com>
2023-02-22 23:03:46 -0600

    Add sort. Level 11 complete.

--- patch/patch-apply.sh
+++ patch/patch-apply.sh
@@ -18,6 +18,12 @@ for submod in ${submods} ; do
     if test -s "${patchdir}/libs/${submod}/build.jam" ; then
         ln -f -s "${patchdir}/libs/${submod}/build.jam" build.jam
     fi
+    if test -d "${rootdir}/libs/${submod}/example" ; then
+        cd "${rootdir}/libs/${submod}/example"
+        if test -s "${patchdir}/libs/${submod}/example/Jamfile.v2" ; then
+            ln -f -s "${patchdir}/libs/${submod}/example/Jamfile.v2" Jamfile.v2
+        fi
+    fi
 done
 rm -f "${rootdir}/libs/algorithm/Jamfile"
 rm -f "${rootdir}/libs/concept_check/Jamfile"
@@ -36,6 +42,7 @@ rm -f "${rootdir}/libs/pool/Jamfile"
 rm -f "${rootdir}/libs/rational/Jamfile"
 rm -f "${rootdir}/libs/safe_numerics/Jamfile.v2"
 rm -f "${rootdir}/libs/signals2/Jamfile"
+rm -f "${rootdir}/libs/sort/Jamfile.v2"
 rm -f "${rootdir}/libs/static_string/Jamfile"
 rm -f "${rootdir}/libs/test/Jamfile"
 rm -f "${rootdir}/libs/url/Jamfile.v2"

039282576db64f04a71fb19f0d54a810a78c2221
Rene Rivera <grafikrobot@gmail.com>
2023-03-05 23:18:48 -0600

    Update submods. And write up a real readme.

--- patch/b2-all.sh
+++ patch/b2-all.sh
@@ -15,7 +15,7 @@ for submod in ${submods} ; do
     echo "============================== ${d}"
     if test -e "build.jam" ; then
         set +e
-        b2 all
+        b2 all define=BOOST_DISABLE_PRAGMA_MESSAGE=1
         set -e
     fi
 done
